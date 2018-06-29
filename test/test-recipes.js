47210a623bc7821bb83a85dda9ec91dbcd544728
MorganHuegel <mhue2910@gmail.com>
2018-06-29 15:24:02 -0400

    added test for PUT requests.  Made adjustments to recipesRouter based on failure of the test

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -48,4 +48,30 @@ describe('recipes', function() {
           Object.assign(newRecipe, {id: response.body.id}));
       });
   });
+
+  it('should edit a recipe with PUT requests', function() {
+    const recipeChange = {name:'fake Name', ingredients:['ing1', 'ing2', 'ing3']};
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(res => {
+        const dummyId = res.body[0].id;
+        recipeChange.id = dummyId;
+
+        return chai.request(app)
+          .put(`/recipes/${dummyId}`)
+          .send(recipeChange)
+          .then(response => {
+            expect(response).to.be.a('object');
+            expect(response).to.be.json;
+            expect(response).to.have.status(200);
+            expect(response.body).to.include.keys(['id', 'name', 'ingredients']);
+            expect(response.body).to.deep.equal(
+              Object.assign(recipeChange, {id: response.body.id})
+            );
+          });
+      });
+
+
+  });
 });
